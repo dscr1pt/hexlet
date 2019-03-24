259f1f884f055f01913ca6d9ebf8ddeb2cfc2367
dscr1pt <48305475+dscr1pt@users.noreply.github.com>
2019-03-25 09:18:32 +1000

    Create ex9.js

---
+++ ex9.js
@@ -0,0 +1,13 @@
+import assert from 'assert';
+import factorial from './implementations';
+
+// Example
+assert.equal(factorial(2), 2);
+assert.equal(factorial(3), 6);
+
+
+// BEGIN (write your solution here)
+assert.equal(factorial(0), 1);
+assert.equal(factorial(1), 1);
+assert.equal(factorial(5), 120);
+// END
